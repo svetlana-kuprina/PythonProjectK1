11f4ca6cffc5eb481b0da34f9f582a1eaa9d353e
svetlana-kuprina <kupsveta@rambler.ru>
2025-11-13 18:08:23 +0500

    К1 edit utils.py

--- PythonProjectK1/src/utils.py
+++ PythonProjectK1/src/utils.py
@@ -2,23 +2,27 @@ import datetime
 from typing import Any
 
 import pandas as pd
+from pandas import DataFrame
 
 
 def open_file() -> Any:
     """Функция чтения excel файла"""
-    excel_data = pd.read_excel('../data/operations.xlsx')
+    excel_data = pd.read_excel('../data/operations.xlsx', sheet_name='Отчет по операциям')
     return excel_data
 
-def time_range(date_times):
-    """Функция обработки времени"""
+def info_kart(date_times:str) -> DataFrame:
+    date_to = datetime.datetime.strptime(date_times, "%Y-%m-%d %H:%M:%S")
+    date_from = date_to.replace(day=1)
+    date_to_str = date_to.strftime("%d.%m.%Y %H:%M:%S")
+    date_from_str = date_from.strftime("%d.%m.%Y %H:%M:%S")
+    excel_data = open_file()
+    excel_data['Дата операции'] = pd.to_datetime(excel_data['Дата операции'],dayfirst=True)
+    # excel_data = excel_data.set_index('Дата операции')
+    # excel_data_range = excel_data.loc[date_from_str:date_to_str]
+    print(date_from_str, date_to_str)
 
-    date_obj = datetime.datetime.strptime(date_times, "%Y-%m-%d %H:%M:%S")
-    date_obj =
-    date_from = date_obj.replace(day=1)
+    excel_data_reviews = excel_data.loc[(excel_data['Дата операции'] > date_from_str) & (excel_data['Дата операции'] < date_to_str)]
 
+    return excel_data_reviews
 
-    return date_from, date_to
-
-def info_kart():
-    pass
-print(time_range('2024-05-14 16:35:27'))
+print(info_kart("2021-12-19 20:13:13"))
--- PythonProjectK1/src/views.py
+++ PythonProjectK1/src/views.py
@@ -1,12 +1,17 @@
 import datetime
-from typing import Any
+import json
+from typing import Any, Dict
+
+from src.utils import info_kart
 
 
-def home_page(date_times:str) ->Any:
-    """Функция формирует JSON файл с данными для главной страницы"""
+def home_page(date_times:str) ->Dict[str,Any]:
+    """Функция формирует JSON файл с данными для главной страницы. Принимает на вход строку с датой и временем открытия,
+    типа 2021-12-19 20:13:13"""
 
-    date_obj = datetime.datetime.strptime(date_times, "%Y-%m-%d %H:%M:%S")
-    hour = date_obj.hour
+    date_to = datetime.datetime.strptime(date_times, "%Y-%m-%d %H:%M:%S")
+    date_from = date_to.replace(day=1)
+    hour = date_to.hour
     if 0 < hour < 5:
         greetings = 'Доброй ночи'
     elif 5 < hour < 11:
@@ -15,6 +20,15 @@ def home_page(date_times:str) ->Any:
         greetings = 'Добрый день'
     else:
         greetings = 'Добрый вечер'
+    data = {
+        "greetings": greetings
+    }
+    json_data = json.dumps(data,ensure_ascii=False, indent=4)
 
-    return greetings
+    print(date_from, date_to)
+
+
+
+    return info_kart(date_from,date_to)
+print(home_page('2021-12-19 20:13:13'))
 
